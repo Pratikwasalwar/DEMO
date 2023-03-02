15bb758ab3b5d5f68f0d3cf2ce9698663bbe28c8
Pratik P. Wasalwar <122254604+Pratikwasalwar@users.noreply.github.com>
2023-03-02 23:50:14 +0530

    Update Data2.java

--- Data2.java
+++ Data2.java
@@ -4,23 +4,6 @@ public class Data2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hi");
-		System.out.println("hello");
-		
-		//1) A scenario where ArithmeticException occurs.
-		/*try{
-			int a = 10/0;
-		}catch(Exception e) {
-			System.out.println(e);
-		}*/
-		
-
-		//1) A scenario where ArithmeticException occurs.
-		/*try{
-			int a = 10/0;
-		}catch(Exception e) {
-			System.out.println(e);
-		}*/
 		
 
 	}
