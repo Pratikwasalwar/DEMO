47ff82e8fec345f213a699473ea362668d132558
Pratik P. Wasalwar <122254604+Pratikwasalwar@users.noreply.github.com>
2023-03-03 09:27:23 +0530

    print statement add

--- Data2.java
+++ Data2.java
@@ -3,7 +3,11 @@ package com.execeptionalhandling;
 public class Data2 {
 
 	public static void main(String[] args) {
-	System.out.println("class is empty");	
+	System.out.println("class is empty");
+	System.out.println("class is empty");
+	System.out.println("class is empty");
+	System.out.println("class is empty");
+		
 		
 
 	}
