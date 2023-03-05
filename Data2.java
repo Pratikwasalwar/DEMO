93c3d12720f2279ed3823e0781fe7bb49ee185ce
Pratik P. Wasalwar <122254604+Pratikwasalwar@users.noreply.github.com>
2023-03-05 17:45:55 +0530

    Update Data2.java

--- Data2.java
+++ Data2.java
@@ -5,8 +5,6 @@ public class Data2 {
 	public static void main(String[] args) {
 	System.out.println("class is empty");
 	System.out.println("class is empty");
-	System.out.println("class is empty");
-	System.out.println("class is empty");
 		
 		
 
